838acbbfc24bcd7f29cb68f1f348116c32f15b8e
SufiyanAnsari07 <sufiyanansari30592@gmail.com>
2025-05-12 21:33:28 +0530

    round robin code fixed

--- cpuscheduler/RRScheduler.java
+++ cpuscheduler/RRScheduler.java
@@ -1,50 +1,81 @@
 package cpuscheduler;
 
-import java.io.*;
-import java.util.*;
+import java.io.* ;
+import java.util.* ;
 
 public class RRScheduler implements Scheduler {
     public void schedule ( ArrayList<Process> processList , BufferedWriter bw , PriorityQueue<AlgoResult> pq ) throws IOException {
-        int timeQuantum = 4 ;
+        int timeQuantum = 2 ;
+        int n = processList.size() ;
+        Queue<Process> readyQueue = new LinkedList<>() ;
+        int[] remainingBurst = new int[n] ;
+        int[] completionTime = new int[n] ;
+        int[] waitingTime = new int[n] ;
+        int[] turnaroundTime = new int[n] ;
+        boolean[] isEnqueued = new boolean[n] ;
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            remainingBurst[i] = processList.get(i).burst ;
+        }
+
         int currentTime = 0 ;
-        int totalWaitingTime = 0 ;
-        int totalTurnaroundTime = 0 ;
+        int completed = 0 ;
 
-        Collections.sort ( processList , new Comparator<Process> () {
-            public int compare ( Process p1 , Process p2 ) {
-                return Integer.compare ( p1.arrival , p2.arrival ) ;
+        while ( completed < n ) {
+            for ( int i = 0 ; i < n ; i++ ) {
+                if ( !isEnqueued[i] && processList.get(i).arrival <= currentTime ) {
+                    readyQueue.offer ( processList.get(i) ) ;
+                    isEnqueued[i] = true ;
+                }
             }
-        } ) ;
-        
-        Queue<Process> queue = new LinkedList<> () ;
 
-        for ( Process p : processList ) {
-            queue.add ( p ) ;
+            if ( readyQueue.isEmpty() ) {
+                currentTime++ ;
+                continue ;
+            }
+
+            Process current = readyQueue.poll() ;
+            int idx = processList.indexOf ( current ) ;
+
+            int execTime = Math.min ( timeQuantum , remainingBurst[idx] ) ;
+            remainingBurst[idx] -= execTime ;
+            currentTime += execTime ;
+
+            for ( int i = 0 ; i < n ; i++ ) {
+                if ( !isEnqueued[i] && processList.get(i).arrival <= currentTime ) {
+                    readyQueue.offer ( processList.get(i) ) ;
+                    isEnqueued[i] = true ;
+                }
+            }
+
+            if ( remainingBurst[idx] > 0 ) {
+                readyQueue.offer ( current ) ;
+            }
+            else {
+                completionTime[idx] = currentTime ;
+                turnaroundTime[idx] = completionTime[idx] - current.arrival ;
+                waitingTime[idx] = turnaroundTime[idx] - current.burst ;
+                completed++ ;
+            }
         }
 
-        while ( !queue.isEmpty() ) {
-            Process p = queue.poll() ;
-            if ( p.remaining <= timeQuantum ) {
-                currentTime += p.remaining ;
-                p.remaining = 0 ;
-                totalTurnaroundTime += currentTime - p.arrival ;
-                totalWaitingTime += currentTime - p.arrival - p.burst ;
+        int totalWaitingTime = 0 , totalTurnaroundTime = 0 ;
 
-                bw.write ( "PID : " + p.pid +
-                        ", Arrival Time : " + p.arrival +
-                        ", Burst Time: " + p.burst +
-                        ", Completion Time : " + currentTime +
-                        ", Waiting Time : " + (currentTime - p.arrival - p.burst) +
-                        ", Turnaround Time : " + (currentTime - p.arrival) + "\n" ) ;
-            } else {
-                p.remaining -= timeQuantum ;
-                currentTime += timeQuantum ;
-                queue.add(p) ;
-            }
+        for ( int i = 0 ; i < n ; i++ ) {
+            Process p = processList.get(i) ;
+            bw.write ( "PID : " + p.pid +
+                       ", Arrival Time : " + p.arrival +
+                       ", Burst Time : " + p.burst +
+                       ", Completion Time : " + completionTime[i] +
+                       ", Waiting Time : " + waitingTime[i] +
+                       ", Turnaround Time : " + turnaroundTime[i] + "\n" ) ;
+            totalWaitingTime += waitingTime[i] ;
+            totalTurnaroundTime += turnaroundTime[i] ;
         }
 
-        bw.write ( "Average Waiting Time: " + ( ( double ) totalWaitingTime / processList.size ( ) ) + "\n" ) ;
-        bw.write ( "Average Turnaround Time: " + ( ( double ) totalTurnaroundTime / processList.size ( ) ) + "\n" ) ;
-        pq.add ( new AlgoResult( "Round Robin Scheduling :", totalWaitingTime , totalTurnaroundTime ) ) ;
+        bw.write ( "Average Waiting Time: " + ( ( double ) totalWaitingTime / n ) + "\n" ) ;
+        bw.write ( "Average Turnaround Time: " + ( ( double ) totalTurnaroundTime / n ) + "\n" ) ;
+
+        pq.add ( new AlgoResult ( "Round Robin Scheduling :" , totalWaitingTime , totalTurnaroundTime ) ) ;
     }
 }
